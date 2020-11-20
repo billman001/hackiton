6087457ec90775f2de5f5817ef4215781addaca7
Nikhil Kushawah <55947969+limitless-Sky@users.noreply.github.com>
2020-11-20 11:49:14 +0530

    Add files via upload
    In this pattern, I had created a Diamond shape for given input.

---
+++ pattern5.java
@@ -0,0 +1,38 @@
+//			*
+//		*	*	*
+//	*	*	*	*	*
+//		*	*	*
+//			*
+//Dimond Pattern for input-3.
+import java.util.Scanner;
+
+public class Pattern5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i, j, space = 1;
+		space = n - 1;
+		for (j = 1; j <= n; j++) {
+			for (i = 1; i <= space; i++) {
+				System.out.print("  ");
+			}
+			space--;
+			for (i = 1; i <= 2 * j - 1; i++) {
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+		space = 1;
+		for (j = 1; j <= n - 1; j++) {
+			for (i = 1; i <= space; i++) {
+				System.out.print("  ");
+			}
+			space++;
+			for (i = 1; i <= 2 * (n - j) - 1; i++) {
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+		sc.close();
+	}
+}
